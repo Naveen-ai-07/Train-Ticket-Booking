008e9c8bdb04def0ffe4650f0af6e5b79798e0c8
Naveen-ai-07 <itsmenaveen13@gmail.com>
2025-05-07 16:02:49 +0530

    Add Netlify deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/Train-Ticket-Booking' : '',
+  }
 };
 
 module.exports = nextConfig; 
